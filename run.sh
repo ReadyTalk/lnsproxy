c7e11b230352aebe698f1cfd23015ee631a34f7c
Brian Kosick <brian.kosick@readytalk.com>
2015-09-30 08:11:19 -0600

    Fix script error

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@ echo "---------------------"
 
 function run {
   echo "${@}"
-  "${@}"
+  "${@}" > /etc/nginx/nginx.conf
 }
 
 run /bin/sed \
   -e "s/LNS_PORT_5000_TCP_ADDR/${LNS_PORT_5000_TCP_ADDR}/g" \
   -e "s/LNS_PORT_5000_TCP_PORT/${LNS_PORT_5000_TCP_PORT:-5000}/g" \
-  /templates/nginx.conf > /etc/nginx/nginx.conf
+  /templates/nginx.conf
 
 nginx
